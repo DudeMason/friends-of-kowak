2457c16228aab497be459b7fc6f1f56c22a61a7a
Mason Eyre <mason.deyre@gmail.com>
2020-10-26 08:31:45 -0600

    Getting the homePage ready!

--- client/src/shared/pages/Home.js
+++ client/src/shared/pages/Home.js
@@ -14,58 +14,41 @@ class Home extends React.Component {
 
 		return (
 			<div>
-				{
-					!edit ?
-					<>
-						<div>
-							{page.text1}
-							<br/>
-							{page.text2}
+				<div className='threeProng'>
+					<div className='one'>
+						<div className='prongHeader'>
+							{!edit ? <>{page.text1}</> : <input name='text1' value={page.text1 ?? ""} onChange={handleChange}/>}
 						</div>
-						<div>
-							{page.text3}
-							<br/>
-							{page.text4}
+						<div className='prongText'>
+							{!edit ? <>{page.text2}</> : <textarea name='text2' value={page.text2 ?? ""} onChange={handleChange}/>}
 						</div>
-						<div>
-							{page.text5}
-							<br/>
-							{page.text6}
+					</div>
+					<div className='two'>
+						<div className='prongHeader'>
+							{!edit ? <>{page.text3}</> : <input name='text3' value={page.text3 ?? ""} onChange={handleChange}/>}
 						</div>
-						<br/>
-						<div>
-							{page.text7}
+						<div className='prongText'>
+							{!edit ? <>{page.text4}</> : <textarea name='text4' value={page.text4 ?? ""} onChange={handleChange}/>}
 						</div>
-						<br/>
-						<div>
-							{page.text8}
+					</div>
+					<div className='three'>
+						<div className='prongHeader'>
+							{!edit ? <>{page.text5}</> : <input name='text5' value={page.text5 ?? ""} onChange={handleChange}/>}
 						</div>
-						<br/>
-						<div>
-							{page.text9}
+						<div className='prongText'>
+							{!edit ? <>{page.text6}</> : <textarea name='text6' value={page.text6 ?? ""} onChange={handleChange}/>}
 						</div>
-					</>
-								:
-					<>
-						<textarea name='text1' value={page.text1 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text2' value={page.text2 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text3' value={page.text3 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text4' value={page.text4 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text5' value={page.text5 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text6' value={page.text6 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text7' value={page.text7 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text8' value={page.text8 ?? ""} onChange={handleChange}/>
-						<br/>
-						<textarea name='text9' value={page.text9 ?? ""} onChange={handleChange}/>
-					</>
-				}
+					</div>
+				</div>
+				<div>
+					{!edit ? <>{page.text7}</> : <textarea name='text7' value={page.text7 ?? ""} onChange={handleChange}/>}
+				</div>
+				<div>
+					{!edit ? <>{page.text8}</> : <textarea name='text8' value={page.text8 ?? ""} onChange={handleChange}/>}
+				</div>
+				<div>
+					{!edit ? <>{page.text9}</> : <textarea name='text9' value={page.text9 ?? ""} onChange={handleChange}/>}
+				</div>
 			</div>
 		);
 	}
